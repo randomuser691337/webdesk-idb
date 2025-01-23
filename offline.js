6055252290427ba8a1f08deb210159feb7b1a0fc
randomuser691337 <randomuser691337@gmail.com>
2025-01-23 00:15:49 -0600

    every breath you take

--- offline.js
+++ offline.js
@@ -1,31 +1,32 @@
-
-const CACHE_NAME = 'WebBoot Beta 2';
-const CACHE_FILES = [
+const CACHE_NAME = 'WebBoot Beta 3';
+const FILES_TO_CACHE = [
     '/go/fs.js',
     '/go/wfs.js',
     '/index.html',
     '/offline.js',
-    '/go/target.json'
+    '/go/target.json',
 ];
 
 self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(CACHE_FILES).catch((error) => {
-                console.error('<!> Failed:', error);
-            });
-        })
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE)).then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) =>
+            Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) return caches.delete(cache);
+                })
+            )
+        ).then(() => self.clients.claim())
     );
 });
 
 self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request).catch(() => {
-                if (event.request.mode === 'navigate') {
-                    return caches.match(OFFLINE_URL);
-                }
-            });
-        })
+        caches.match(event.request).then((cachedResponse) => cachedResponse || fetch(event.request))
     );
 });
